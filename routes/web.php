df624af87466ada82095112b04d4ca334c0b7a2f
vitou-IT <vitou_pak@phillipinsurance.com.kh>
2026-04-25 12:45:01 +0700

    feature(backend): add sign route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signing', function () {
+//    signing
+    return response()->json([
+        'url' => URL::signedRoute('email.action', ['user' => 123])
+    ]);
+});
+
+Route::get('/action', function (Request $request) {
+    if (!$request->hasValidSignature()) {
+        abort(403);
+    }
+
+    // safe to proceed
+    return response()->json([
+        'message' => 'Signed route found'
+    ]);
+
+})->name('email.action');
+
