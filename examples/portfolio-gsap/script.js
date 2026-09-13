60863ac74ee10a7d950dee9f352dca98f5b0dcf4
vitou <pakvitou168@gmail.com>
2026-09-13 17:34:32 +0700

    Personal Name and Smooth Scroll

--- examples/portfolio-gsap/script.js
+++ examples/portfolio-gsap/script.js
@@ -1,5 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const lenis = new Lenis();
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => lenis.raf(time * 1000));
+gsap.ticker.lagSmoothing(0);
+
 gsap.from(".hero-title", { y: 30, opacity: 0, duration: 1 });
 gsap.from(".hero-sub", { y: 20, opacity: 0, duration: 1, delay: 0.3 });
 
